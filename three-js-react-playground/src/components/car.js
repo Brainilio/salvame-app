a08cde807bf35a1ffd6d55d67e8740ddc22989cb
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-06-13 12:21:29 +0200

    gltf loader works

--- three-js-react-playground/src/components/car.js
+++ three-js-react-playground/src/components/car.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from "react"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 const Car = () => {
-	const loader = new GLTFLoader()
-
 	const [model, setModel] = useState()
+
 	useEffect(() => {
-		loader.load("../../public/assets/car/scene.gltf", setModel)
+		new GLTFLoader().load("assets/jet/scene.gltf", setModel)
 	})
-	console.log(model)
-	return model ? <primitive object={model} /> : null
+
+	return model ? <primitive object={model.scene} /> : null
 }
 
 export default Car
